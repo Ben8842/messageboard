dd0d63695d9142394a517e6bc90176ea5724276b
Ben8842 <z87e99b44r55a@gmail.com>
2021-02-03 12:18:27 -0500

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+class MovieForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieName: "",
+      count: 0,
+      method: "",
+    };
+  }
+
+  handleForm = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    console.log(name);
+    console.log(value);
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form>
+        <h1> The movie name is {this.state.movieName}</h1>
+        <p>Enter a movie name below.</p>
+        <input type="text" name="movieName" onChange={this.handleForm} />
+        <button type="button" name="submit" onChange={this.handleForm}>
+          submit
+        </button>
+
+        <p>Enter the watching method. </p>
+        <input type="text" name="method" onChange={this.handleForm} />
+        <h3>The watching method is {this.state.method}</h3>
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Hello World</header>
+
+      <div>
+        <MovieForm />
+      </div>
     </div>
   );
 }
